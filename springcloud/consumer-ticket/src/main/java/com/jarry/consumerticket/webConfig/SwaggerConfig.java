57264c27d64fcd4d4ab1ae6379698ed89ca341ac
changjiarong <1049342874@qq.com>
2020-01-15 17:45:57 +0800

    eureka安全SS

--- springcloud/consumer-ticket/src/main/java/com/jarry/consumerticket/webConfig/SwaggerConfig.java
+++ springcloud/consumer-ticket/src/main/java/com/jarry/consumerticket/webConfig/SwaggerConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @BelongsProject: spring_cloud_demo
@@ -23,12 +29,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name("token")
+                .description("令牌token")
+                .modelRef(new ModelRef("String"))
+                .parameterType("query")
+                .required(true)
+                .build();
+        List<Parameter> pars = new ArrayList<>();
+        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.jarry.consumerticket.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
